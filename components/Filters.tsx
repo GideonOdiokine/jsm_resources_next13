aa78efaf9f99e4e668f1b467327e613c58bee069
GideonOdiokine <gidysmartodiokineh@gmail.com>
2023-12-16 05:05:29 +0100

    added loading page and filters functionality

--- components/Filters.tsx
+++ components/Filters.tsx
@@ -1,21 +1,47 @@
 "use client";
-import React, { useState } from "react";
 
-const links = ["all", "Next 13", "frontend", " backend", "fullstack"];
+import { formUrlQuery } from "@/sanity/utils";
+import { useState } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+
+const links = ["all", "Next 13", "frontend", "backend", "fullstack"];
 
 const Filters = () => {
-  const [activeLink, setActiveLink] = useState("all");
+  const [active, setActive] = useState("");
+  const searchParms = useSearchParams();
+  const router = useRouter();
+
   const handleFilter = (link: string) => {
-    setActiveLink(link);
+    let newUrl = "";
+
+    if (active === link) {
+      setActive("");
+
+      newUrl = formUrlQuery({
+        params: searchParms.toString(),
+        keysToRemove: ["category"],
+      });
+    } else {
+      setActive(link);
+
+      newUrl = formUrlQuery({
+        params: searchParms.toString(),
+        key: "category",
+        value: link.toLowerCase(),
+      });
+    }
+
+    router.push(newUrl, { scroll: false });
   };
+
   return (
     <ul className="text-white-800 body-text no-scrollbar flex w-full max-w-full gap-2 overflow-auto py-12 sm:max-w-2xl">
       {links.map((link) => (
         <button
-          onClick={() => handleFilter(link)}
           key={link}
+          onClick={() => handleFilter(link)}
           className={`${
-            activeLink === link ? "gradient_blue-purple" : ""
+            active === link ? "gradient_blue-purple" : ""
           } whitespace-nowrap rounded-lg px-8 py-2.5 capitalize`}
         >
           {link}
