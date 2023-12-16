0fbbe7cab4ecef3b52781ff0f4553935a1f8bac9
GideonOdiokine <gidysmartodiokineh@gmail.com>
2023-12-16 05:07:16 +0100

    added searchForm

--- components/SearchForm.tsx
+++ components/SearchForm.tsx
@@ -1,10 +1,42 @@
-"use client"
-import Image from 'next/image'
-import React, { useState } from 'react'
-import { Input } from './ui/input';
+"use client";
+
+import { usePathname, useRouter, useSearchParams } from "next/navigation";
+import { useState, useEffect } from "react";
+import Image from "next/image";
+
+import { Input } from "@/components/ui/input";
+import { formUrlQuery } from "@/sanity/utils";
 
 const SearchForm = () => {
-    const [searchTerm, setSearchTerm] = useState("")
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const pathname = usePathname();
+
+  const [search, setSearch] = useState("");
+
+  useEffect(() => {
+    const delayDebounceFn = setTimeout(() => {
+      let newUrl = "";
+
+      if (search) {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          key: "query",
+          value: search,
+        });
+      } else {
+        newUrl = formUrlQuery({
+          params: searchParams.toString(),
+          keysToRemove: ["query"],
+        });
+      }
+
+      router.push(newUrl, { scroll: false });
+    }, 300);
+
+    return () => clearTimeout(delayDebounceFn);
+  }, [search]);
+
   return (
     <form className="flex-center mx-auto mt-10 w-full sm:-mt-10 sm:px-5">
       <label className="flex-center relative w-full max-w-3xl">
@@ -19,12 +51,12 @@ const SearchForm = () => {
           className="base-regular h-fit border-0 bg-black-400 py-6 pl-20 pr-8 text-white-800 !ring-0 !ring-offset-0 placeholder:text-white-800"
           type="text"
           placeholder="Search"
-          value={searchTerm}
-          onChange={(e) => setSearchTerm(e.target.value)}
+          value={search}
+          onChange={(e) => setSearch(e.target.value)}
         />
       </label>
     </form>
   );
-}
+};
 
-export default SearchForm
+export default SearchForm;
