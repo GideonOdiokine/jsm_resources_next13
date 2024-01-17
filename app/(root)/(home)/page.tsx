279737092d3ada3c74a4935a75de80c980ba8589
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-01-18 00:25:49 +0100

    add text

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -26,7 +26,7 @@ const Page = async ({ searchParams }: Props) => {
       <section className="nav-padding w-full">
         <div className="flex-center relative min-h-[274px] w-full flex-col rounded-xl bg-banner bg-cover bg-center text-center">
           <h1 className="sm:heading1 heading2 mb-6 text-center text-white">
-            JavaScript Mastery Resources
+            Gidysmart Mastery Resources
           </h1>
         </div>
         <SearchForm />
