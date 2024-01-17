e45750947d2be780dee1d74f294fc768f628b7d0
GideonOdiokine <gidysmartodiokineh@gmail.com>
2024-01-18 00:37:15 +0100

    removed image

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -5,8 +5,9 @@ const Navbar = () => {
   return (
     <nav className="flex-center fixed top-0 z-50 w-full border-b-2 border-black-200 bg-black-100 py-7 text-white">
       <div className="flex-between mx-auto w-full max-w-screen-2xl px-6 xs:px-8 sm:px-16">
-        <Link href="/">
-          <Image src="/jsm-logo.svg" width={55} height={40} alt="JSM logo" />
+        <Link href="/" className='text-[30px] font-poppins '>
+          {/* <Image src="/jsm-logo.svg" width={55} height={40} alt="JSM logo" /> */}
+          GO
         </Link>
 
         <Image
